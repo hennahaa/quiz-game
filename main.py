5a7e1ce1b1d9ad127b28873a1019e2e2ca6fb460
Henna Haapala <henna.haapala@tuni.fi>
2021-10-25 15:22:44 +0300

    Yhdistettiin henna ja ville branch ja yhdistettiin peli

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ maksimi_pisteet = len(kysymykset)
 def ohje():
     print("Ohjelmassa voit valita joko 0 tai 1, 0 lopettaa pelin ja 1 jatkaa peliä.")
 
-def pelaa():
+def peli1():
     i = 0
     pisteet = 0
     for kysymys in kysymykset:
@@ -37,6 +37,36 @@ def pelaa():
         i += 1
     print(f"Sait {pisteet}/{maksimi_pisteet} pistettä!")
 
+def peli2():
+
+    pisteet = 0
+
+    print("Vastaa k tai e, mikä tahansa muu lopettaa.")
+
+    kysymys1 = input("Onko Thaikuissa kivaa (k/e): ")
+    if kysymys1 == "k":
+        print("Oikein!")
+        pisteet += 1
+    else:
+        print("Väärin!")
+
+    kysymys2 = input("Onko C# parempi kuin Python (k/e): ")
+    if kysymys2 == "e":
+        print("Oikein!")
+        pisteet += 1
+    else:
+        print("Väärin!")
+
+
+    kysymys3 = input("Onko GCP parasta ikinä (k/e): ")
+    if kysymys3 == "k":
+        print("Oikein!")
+        pisteet += 1
+    else:
+        print("Väärin!")
+
+    print(f"Sait {pisteet} pistettä!")
+
 #while-loop joka kutsuu kysymyksentulostusta, käsittelee pelaajan syötteen
 #1 pelaa 0 lopeta
 def suorita():
@@ -45,7 +75,13 @@ def suorita():
         print()
         kysymys = int(input("Oletko valmis pelaamaan? 1 = Pelaa, 0 = Lopeta "))
         if kysymys == 1:
-            pelaa()
+            peli = int(input("Mitä peliä pelataan? Peli1, peli2 vai peli3? "))
+            if peli == 1:
+              peli1()
+            if peli == 2:
+              peli2()
+            if peli == 3:
+              pass
         elif kysymys == 0:
             break
         else:
